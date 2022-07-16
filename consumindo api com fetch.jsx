22327ee15d172731160ceb52822b14c5458f2d0d
Lauricio De Souza <86666254+LauricioX@users.noreply.github.com>
2022-07-16 16:58:13 -0300

    Update and rename consumindo api com fetch to consumindo api com fetch.jsx

---
+++ consumindo api com fetch.jsx
@@ -0,0 +1,27 @@
+useEffect(()=>{
+    fetch('https://api-cfb.lauriciomalek.repl.co')
+    .then(res=>res.json())
+    .then((res)=>{
+        setCarros(res)
+    })
+})
+
+
+//para usar basta criar um state e passa como paratro um array vazio
+//EX
+div
+const [nome,setNome] = useState([])
+
+// depois basta usar dentro do html usando o ponto map para iterar sobre cado objeto 
+// obtido da API
+//ex
+
+return(
+<div>
+    {nome.map(nome => (
+         <h3>{nome.marca}</h3>
+         ))}
+</div>
+);
+
+// se chegou até aqui você ja provou
